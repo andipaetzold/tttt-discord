2f670f033f32d8eff90430c53bdc1f67b9d5f3ff
Andi Pätzold <github@andipaetzold.com>
2021-02-06 18:24:18 +0100

    log list of server on startup

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,24 @@ import { log } from "./log";
 
 dotenv.config();
 
-client.once("ready", () => {
-    client.user?.setActivity({
-        name: "Zwift",
-        type: "WATCHING",
-    });
-    log("Ready", "Server");
-});
+client.once("ready", ready);
 client.once("reconnecting", () => log("Reconnecting", "Server"));
 client.once("disconnect", () => log("Disconnect", "Server"));
 client.on("error", (e) => console.error("Error", e));
 client.on("message", handleMessage);
 
 client.login(process.env.DISCORD_TOKEN);
+
+function ready() {
+    client.user!.setActivity({
+        name: "Zwift",
+        type: "WATCHING",
+    });
+
+    log(`Member of ${client.guilds.cache.size} server(s)`, "Server");
+    client.guilds.cache.forEach((guild) => {
+        log(`${guild.id}: ${guild.name}`, "Server");
+    });
+
+    log("Ready", "Server");
+}
