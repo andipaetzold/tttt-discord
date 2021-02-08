599730f73ce6a7c850ddc6ffd208983596e22502
Andi Pätzold <github@andipaetzold.com>
2021-02-08 20:12:24 +0100

    add logging to guildDelete

--- src/handlers/guildCreate/index.ts
+++ src/handlers/guildCreate/index.ts
@@ -2,5 +2,5 @@ import type { Guild } from "discord.js";
 import { log } from "../../services/log";
 
 export async function handleGuildCreate(guild: Guild) {
-    log(`Joined ${guild.id}: ${guild.name}`, "Server");
+    log(`Joined ${guild.name}`, `G:${guild.id}`);
 }
--- src/handlers/guildDelete/index.ts
+++ src/handlers/guildDelete/index.ts
@@ -1,7 +1,9 @@
 import { Guild } from "discord.js";
 import { removeConfig } from "../../config";
+import { log } from "../../services/log";
 import { stopTimer } from "../../timer";
 
 export async function handleGuildDelete(guild: Guild) {
+    log(`Left ${guild.name}`, `G:${guild.id}`);
     await Promise.all([stopTimer(guild.id), removeConfig(guild.id)]);
 }
