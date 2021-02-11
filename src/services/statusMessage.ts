a96e397c21ac060b830a2ab6fcc850a0cebdb47b
Andi Pätzold <github@andipaetzold.com>
2021-02-11 19:33:57 +0100

    improve status message and its error handling

--- src/services/statusMessage.ts
+++ src/services/statusMessage.ts
@@ -1,10 +1,13 @@
-import { MessageEmbed, TextChannel } from "discord.js";
+import { Message, MessageEmbed, TextChannel } from "discord.js";
 import { client } from "../discord";
 import { getConfig } from "../persistence/config";
 import { getAllTimers, getTimer, setTimer } from "../persistence/timer";
 import type { Config, Timer } from "../types";
 import { EMOJI_PLUS10, EMOJI_SKIP, EMOJI_TOAST } from "../util/emojis";
+import { log } from "./log";
 import { getNextAthleteIndex } from "./timer";
+
+const DEFAULT_FOOTER = "Use `!t stop` to stop the timer.";
 
 export function createStatusMessage(config: Config, timer: Timer): MessageEmbed {
     const currentAthlete = config.athletes[timer.currentAthleteIndex];
@@ -17,18 +20,18 @@ export function createStatusMessage(config: Config, timer: Timer): MessageEmbed 
             .setTitle(`${currentAthlete.name} (${currentAthlete.time}s)`)
             .addField("Next athlete", `${nextAthlete.name} (${nextAthlete.time}s)`)
             .setFooter(
-                `Click ${EMOJI_PLUS10} to add 10 seconds and ${EMOJI_SKIP} to go to the next rider. Click ${EMOJI_TOAST} when you are dead.`
+                `Click ${EMOJI_PLUS10} to add 10 seconds and ${EMOJI_SKIP} to go to the next rider. Click ${EMOJI_TOAST} when you are dead.\n${DEFAULT_FOOTER}`
             );
     } else {
         messageEmbed = new MessageEmbed()
             .setTitle("Waiting for the start...")
             .addField("First athlete", `${currentAthlete.name} (${currentAthlete.time}s)`)
             .setFooter(
-                `Click ${EMOJI_PLUS10} to add 10 seconds and ${EMOJI_SKIP} to start. Click ${EMOJI_TOAST} when you are dead.`
+                `Click ${EMOJI_PLUS10} to add 10 seconds and ${EMOJI_SKIP} to start. Click ${EMOJI_TOAST} when you are dead.\n${DEFAULT_FOOTER}`
             );
     }
 
-    return messageEmbed.addField(
+    messageEmbed.addField(
         "Toasted athletes",
         timer.disabledAthletes.length === 0
             ? "*Everybody's still fresh*"
@@ -37,18 +40,27 @@ export function createStatusMessage(config: Config, timer: Timer): MessageEmbed 
                   .map((a) => `• ${a.name}`)
                   .join("\n")
     );
+
+    return messageEmbed;
 }
 
 export async function sendStatusMessage(channel: TextChannel) {
-    const [config, timer] = await Promise.all([getConfig(channel.guild.id), getTimer(channel.guild.id)]);
+    const guildId = channel.guild.id;
+    const [config, timer] = await Promise.all([getConfig(guildId), getTimer(guildId)]);
     if (timer === undefined) {
         return;
     }
 
-    const message = await channel.send(createStatusMessage(config, timer));
-    message.react(EMOJI_PLUS10);
-    message.react(EMOJI_SKIP);
-    message.react(EMOJI_TOAST);
+    let message: Message;
+    try {
+        message = await channel.send(createStatusMessage(config, timer));
+        message.react(EMOJI_PLUS10);
+        message.react(EMOJI_SKIP);
+        message.react(EMOJI_TOAST);
+    } catch (e) {
+        log(e.toString(), `G:${guildId}`, "ERROR");
+        throw new Error("Could not update status message");
+    }
 
     await setTimer({
         ...timer,
@@ -61,26 +73,23 @@ export async function sendStatusMessage(channel: TextChannel) {
 
 export async function updateStatusMessage(guildId: string) {
     const [config, timer] = await Promise.all([getConfig(guildId), getTimer(guildId)]);
-    if (timer === undefined) {
+    if (timer?.status === undefined) {
         return;
     }
 
-    if (timer.status === undefined) {
-        return;
+    try {
+        const channel = (await client.channels.fetch(timer.status.channelId)) as TextChannel;
+        const message = await channel.messages.fetch(timer.status.messageId);
+        await message.edit(createStatusMessage(config, timer));
+    } catch (e) {
+        log(e.toString(), `G:${guildId}`, "ERROR");
+        throw new Error("Could not update status message");
     }
-
-    const channel = (await client.channels.fetch(timer.status.channelId)) as TextChannel;
-    const message = await channel.messages.fetch(timer.status.messageId);
-    await message.edit(createStatusMessage(config, timer));
 }
 
 export async function deleteStatusMessage(guildId: string) {
     const timer = await getTimer(guildId);
-    if (timer === undefined) {
-        return;
-    }
-
-    if (timer.status === undefined) {
+    if (timer?.status === undefined) {
         return;
     }
 
@@ -88,8 +97,9 @@ export async function deleteStatusMessage(guildId: string) {
         const channel = (await client.channels.fetch(timer.status.channelId)) as TextChannel;
         const message = await channel.messages.fetch(timer.status.messageId);
         await message.delete();
-    } catch {
-        // ignore errors
+    } catch (e) {
+        log(e.toString(), `G:${guildId}`, "ERROR");
+        throw new Error("Could not delete status message");
     }
 }
 
@@ -99,10 +109,15 @@ export async function deleteStatusMessage(guildId: string) {
 export async function fetchStatusMessages() {
     const timers = await getAllTimers();
 
-    for (const timer of timers) {
-        if (timer?.status) {
-            const channel = (await client.channels.fetch(timer.status.channelId)) as TextChannel;
-            channel.messages.fetch(timer.status.messageId);
+    for (const timer of timers
+        .filter((timer): timer is Timer => timer !== undefined)
+        .filter((timer) => timer.status !== undefined)) {
+        try {
+            const channel = (await client.channels.fetch(timer.status!.channelId)) as TextChannel;
+            channel.messages.fetch(timer.status!.messageId);
+        } catch (e) {
+            log("Error fetching status message", `G:${timer!.guildId}`, "ERROR");
+            log(e.toString(), `G:${timer!.guildId}`, "ERROR");
         }
     }
 }
