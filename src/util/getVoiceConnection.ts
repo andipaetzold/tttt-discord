a0593f2ac8a4924940613ff0cc7ba25103afed70
Andi Pätzold <github@andipaetzold.com>
2021-02-11 18:41:48 +0100

    check voice channel permissions before joining

--- src/util/getVoiceConnection.ts
+++ src/util/getVoiceConnection.ts
@@ -2,6 +2,7 @@ import { VoiceChannel, VoiceConnection } from "discord.js";
 import { client } from "../discord";
 import { setConfig } from "../persistence/config";
 import { log } from "../services/log";
+import { hasVoicePermissions } from "../services/permissions";
 import { Config } from "../types";
 
 export async function getVoiceConnection(
@@ -15,7 +16,7 @@ export async function getVoiceConnection(
     if (connection === undefined) {
         const voiceChannel = userVoiceChannel;
         if (voiceChannel) {
-            connection = await voiceChannel.join();
+            connection = await checkAndJoin(voiceChannel);
         }
     }
 
@@ -24,7 +25,7 @@ export async function getVoiceConnection(
             const channel = await client.channels.fetch(config.voiceChannelId);
             if (channel.type === "voice") {
                 const voiceChannel = channel as VoiceChannel;
-                connection = await voiceChannel.join();
+                connection = await checkAndJoin(voiceChannel);
             }
         }
     }
@@ -33,7 +34,7 @@ export async function getVoiceConnection(
         const voiceChannels = guild.channels.cache.filter((channel) => channel.type === "voice");
         if (voiceChannels.size === 1) {
             const voiceChannel = voiceChannels.first()! as VoiceChannel;
-            connection = await voiceChannel.join();
+            connection = await checkAndJoin(voiceChannel);
         }
     }
 
@@ -49,4 +50,10 @@ export async function getVoiceConnection(
     }
 
     return connection;
+}
+
+async function checkAndJoin(voiceChannel: VoiceChannel) {
+    if (hasVoicePermissions(voiceChannel.guild)) {
+        return await voiceChannel.join();
+    }
 }
