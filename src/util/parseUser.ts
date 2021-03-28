6cc1a96b65b36b398813b25ade473118e192f19b
Andi Pätzold <github@andipaetzold.com>
2021-03-28 16:33:30 +0200

    refactor: use displayName for mentioned users

--- src/util/parseUser.ts
+++ src/util/parseUser.ts
@@ -4,11 +4,12 @@ import { Athlete } from "../types";
 export default function parseUser(s: string, messageMentions: MessageMentions): Pick<Athlete, "name" | "userId"> {
     if (s.startsWith(`<@`) && s.endsWith(`>`)) {
         const userId = s.slice(s.startsWith(`<@!`) ? 3 : 2, -1);
+        const guildMember = messageMentions.members!.find((member) => member.id === userId)!;
 
-        const guildMember = messageMentions.members!.find((member) => member.user.id === userId)!;
-        const name = guildMember.nickname ?? guildMember.user.username;
-
-        return { name, userId };
+        return {
+            name: guildMember.displayName,
+            userId,
+        };
     } else {
         return {
             name: s,
