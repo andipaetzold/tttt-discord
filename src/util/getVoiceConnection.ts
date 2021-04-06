e9d7fb0f66729507e3cdb7e90e372f9f22a63980
Andi Pätzold <github@andipaetzold.com>
2021-04-06 22:21:43 +0200

    fix: only join channel if its joinable

--- src/util/getVoiceConnection.ts
+++ src/util/getVoiceConnection.ts
@@ -15,7 +15,7 @@ export async function getVoiceConnection(
     if (connection === undefined) {
         const voiceChannel = userVoiceChannel;
         if (voiceChannel) {
-            connection = await voiceChannel.join();
+            connection = await joinIfJoinable(voiceChannel);
         }
     }
 
@@ -24,7 +24,7 @@ export async function getVoiceConnection(
             const channel = await client.channels.fetch(config.voiceChannelId);
             if (channel.type === "voice") {
                 const voiceChannel = channel as VoiceChannel;
-                connection = await voiceChannel.join();
+                connection = await joinIfJoinable(voiceChannel);
             }
         }
     }
@@ -33,7 +33,7 @@ export async function getVoiceConnection(
         const voiceChannels = guild.channels.cache.filter((channel) => channel.type === "voice");
         if (voiceChannels.size === 1) {
             const voiceChannel = voiceChannels.first()! as VoiceChannel;
-            connection = await voiceChannel.join();
+            connection = await joinIfJoinable(voiceChannel);
         }
     }
 
@@ -49,4 +49,11 @@ export async function getVoiceConnection(
     }
 
     return connection;
+}
+
+async function joinIfJoinable(channel: VoiceChannel): Promise<VoiceConnection | undefined> {
+    if (channel.joinable) {
+        return await channel.join();
+    }
+    return undefined;
 }
