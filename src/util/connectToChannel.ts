381eb38c0d631542a4e8da4e9f1edf9de1eb2dc8
Andi Pätzold <github@andipaetzold.com>
2021-10-04 20:56:53 +0200

    fix: log when joining VC

--- src/util/connectToChannel.ts
+++ src/util/connectToChannel.ts
@@ -1,6 +1,7 @@
 import { VoiceChannel } from "discord.js";
 import { joinVoiceChannel, entersState, VoiceConnectionStatus, VoiceConnection } from "@discordjs/voice";
 import { BOT_ID } from "../constants";
+import logger from "../services/logger";
 
 export async function connectToChannel(channel: VoiceChannel): Promise<VoiceConnection | undefined> {
     if (!channel.joinable) {
@@ -16,6 +17,7 @@ export async function connectToChannel(channel: VoiceChannel): Promise<VoiceConn
 
     try {
         await entersState(connection, VoiceConnectionStatus.Ready, 3_000);
+        logger.info(channel.guildId, `Joined VS:${channel.id}`);
         return connection;
     } catch (error) {
         try {
