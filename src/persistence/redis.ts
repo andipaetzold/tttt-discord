be097eeb3157c6c746161be998fea35d28bc19d6
Andi Pätzold <github@andipaetzold.com>
2021-12-06 01:14:34 +0100

    fix: connect redis after creating client

--- src/persistence/redis.ts
+++ src/persistence/redis.ts
@@ -4,13 +4,19 @@ import { REDIS_URL } from "../constants";
 const client = createClient({
     url: REDIS_URL,
 });
+const connected = (async () => {
+    await client.connect();
+    await client.ping();
+})();
 
 export async function write<T = any>(key: string, value: T): Promise<void> {
     const stringified = JSON.stringify(value);
+    await connected;
     await client.set(key, stringified);
 }
 
 export async function read<T = any>(key: string): Promise<T | undefined> {
+    await connected;
     const value = await client.get(key);
     return value ? JSON.parse(value) : undefined;
 }
@@ -20,18 +26,22 @@ export async function readMany<T = any>(keys: string[]): Promise<(T | undefined)
         return [];
     }
 
+    await connected;
     const values = await client.mGet(keys);
     return values.map((value) => (value ? JSON.parse(value) : undefined));
 }
 
 export async function keys(pattern: string): Promise<string[]> {
+    await connected;
     return await client.keys(pattern);
 }
 
 export async function remove(key: string): Promise<void> {
+    await connected;
     await client.del(key);
 }
 
 export async function exists(key: string): Promise<boolean> {
+    await connected;
     return await client.exists(key);
 }
