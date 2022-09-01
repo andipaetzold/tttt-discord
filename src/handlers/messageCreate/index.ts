628e4557fee37f322e1084a1bea2118f5ab7c3e5
Andi Pätzold <github@andipaetzold.com>
2022-09-02 00:57:47 +0200

    fix: do not reply to bots

--- src/handlers/messageCreate/index.ts
+++ src/handlers/messageCreate/index.ts
@@ -3,6 +3,11 @@ import { SLASH_COMMAND } from "../../constants";
 import { HandlerProps } from "../../services/sentry";
 
 export async function handleMessageCreate({ args: [message] }: HandlerProps<[Message]>) {
+    if (message.author.bot) {
+        // ignore bot messages
+        return;
+    }
+
     if (!message.member) {
         message.channel.send(
             `The timer can only be on servers/guilds - not in direct messages. Add me to a server/guild and type \`/${SLASH_COMMAND["name"]} help\` for more details.`
