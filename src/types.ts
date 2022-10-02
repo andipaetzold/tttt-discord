3dc63e595590c6f8b2eaeb2a64ec8bbda9822f12
Andi Pätzold <github@andipaetzold.com>
2022-10-02 23:54:17 +0200

    fix: remove lastSlashCommandMigrationNotification

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,6 @@ export interface Config {
     startDelay: number;
     athletes: Athlete[];
     languageKey: LanguageKey;
-    lastSlashCommandMigrationNotification?: number;
 }
 
 export interface Athlete {
