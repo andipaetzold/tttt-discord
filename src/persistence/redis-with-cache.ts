32ce5b59d37d12632e7ab171d73166851a9de3cd
Andi Pätzold <github@andipaetzold.com>
2023-11-09 01:04:57 +0100

    refactor: use `Map` for redis cache

--- src/persistence/redis-with-cache.ts
+++ src/persistence/redis-with-cache.ts
@@ -5,31 +5,27 @@ import { RedisClient } from "./redis";
  * Assumes redis is not changed by other processes.
  */
 class RedisClientWithCache extends RedisClient {
-    #cache: Record<string, any> = {};
-
-    constructor() {
-        super();
-    }
+    #cache = new Map<string, any>();
 
     async write<T = any>(key: string, value: T): Promise<void> {
         await super.write<T>(key, value);
-        this.#cache[key] = value;
+        this.#cache.set(key, value);
     }
 
     async read<T = any>(key: string): Promise<T | undefined> {
-        if (key in this.#cache) {
-            return this.#cache[key];
+        if (this.#cache.has(key)) {
+            return this.#cache.get(key);
         }
 
         const redisResult = await super.read<T>(key);
         if (redisResult !== undefined) {
-            this.#cache[key] = redisResult;
+            this.#cache.set(key, redisResult);
         }
         return redisResult;
     }
 
     async readMany<T = any>(keys: string[]): Promise<(T | undefined)[]> {
-        const cachedValues = keys.map((key) => this.#cache[key]);
+        const cachedValues = keys.map((key) => this.#cache.get(key));
         if (cachedValues.every((value) => value !== undefined)) {
             return cachedValues.map(([, value]) => value);
         }
@@ -37,23 +33,19 @@ class RedisClientWithCache extends RedisClient {
         const redisResults = await super.readMany<T>(keys);
         for (const [i, redisResult] of redisResults.entries()) {
             if (redisResult !== undefined) {
-                this.#cache[keys[i]] = redisResult;
+                this.#cache.set(keys[i], redisResult);
             }
         }
         return redisResults;
     }
 
-    async keys(pattern: string): Promise<string[]> {
-        return await super.keys(pattern);
-    }
-
     async remove(key: string): Promise<void> {
         await super.remove(key);
-        delete this.#cache[key];
+        this.#cache.delete(key);
     }
 
     async exists(key: string): Promise<boolean> {
-        if (key in this.#cache) {
+        if (this.#cache.has(key)) {
             return true;
         }
 
