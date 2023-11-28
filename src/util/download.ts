be5ea0c9f534b0e70640e23b02370bf42e50dce3
Andi Pätzold <github@andipaetzold.com>
2023-11-28 23:52:52 +0100

    refactor: use fetch API

--- src/util/download.ts
+++ src/util/download.ts
@@ -1,25 +1,21 @@
-import https from "https";
 import fs from "fs";
 import crypto from "crypto";
 import os from "os";
 import path from "path";
+import { Readable } from "stream";
+import { finished } from "stream/promises";
 
 export async function download(url: string): Promise<string> {
     const hash = crypto.createHash("md5").update(url).digest("hex");
     const filename = path.resolve(os.tmpdir(), hash);
 
     if (!fs.existsSync(filename)) {
-        await new Promise<void>((resolve, reject) => {
-            https.get(url, (response) => {
-                if (response.statusCode === 200) {
-                    var file = fs.createWriteStream(filename);
-                    response.pipe(file);
-                    resolve();
-                } else {
-                    reject();
-                }
-            });
-        });
+        const response = await fetch(url);
+        if (!response.ok || !response.body) {
+            throw new Error(`Error fetching url "${url}"`);
+        }
+        const stream = Readable.fromWeb(response.body).pipe(fs.createWriteStream(filename));
+        await finished(stream);
     }
 
     return filename;
