544f32c5b1e8fbb3a553a0721a9d40346e3eb7e1
Andi Pätzold <github@andipaetzold.com>
2023-11-30 17:38:56 +0100

    chore: styling

--- src/handlers/interactionCreate/plus.ts
+++ src/handlers/interactionCreate/plus.ts
@@ -3,7 +3,7 @@ import { SLASH_COMMAND } from "../../constants";
 import { timerExists } from "../../persistence/timer";
 import { addTimeToCurrentAthlete } from "../../services/timer";
 import { isValidDelay } from "../../util/isValidDelay";
-import {updateStatusMessage} from "../../services/statusMessage";
+import { updateStatusMessage } from "../../services/statusMessage";
 
 export async function plus(interaction: ChatInputCommandInteraction): Promise<void> {
     const guildId = interaction.guild!.id;
